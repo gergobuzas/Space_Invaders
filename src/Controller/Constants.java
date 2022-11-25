6670430c752b7bd98c0a11929a864080115b4456
Gergo Buzas <buzasgergo0615@gmail.com>
2022-11-25 13:41:10 +0100

    Added javadoc comments

--- src/Controller/Constants.java
+++ src/Controller/Constants.java
@@ -12,18 +12,19 @@ public class Constants {
     public static int BORDER_LEFT = 5;
 
     public static int GROUND = 900;
-    public static int ALIEN_BULLET_HEIGHT = 60;
-    public static int ALIEN_BULLET_WIDTH = 20;
 
     public static int ALIEN_HEIGHT = 100;
     public static int ALIEN_WIDTH = 100;
     public static int ALIEN_INIT_X = 150;
     public static int ALIEN_INIT_Y = 5;
+    public static int ALIEN_BULLET_HEIGHT = 20;
+    public static int ALIEN_BULLET_WIDTH = 20;
 
     public static int SPACESHIP_WIDTH = 100;
     public static int SPACESHIP_HEIGHT = 100;
     public static int SPACESHIP_INIT_X = 450;
     public static int SPACESHIP_INIT_Y = 800;
+
 
     public static int MOVE_DOWN = 6;
     public static int MOVE_UP = -6;
@@ -32,5 +33,5 @@ public class Constants {
 
     public static int BOMB_FREQ = 1000;
     public static int NUMBER_OF_ALIENS_TO_DESTROY = 24;
-    public static int DELAY = 17;
+    public static int DELAY = 16;
 }
